dc76afd90715220324646e873957bdd4a59254ca
Daniel <weyichen@seas.upenn.edu>
2013-10-10 16:41:06 -0400

    starting over with new railsinstaller. hope this works!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YichenweiApp::Application.config.secret_token = '1815d34db7c3b07c58414f52fadf6bae272a7b859630a57972439d406ef3100656d3a7fd15a758c716dba08a7265b495027df3774d5d853199771256e56efc0f'
