656a3776aeb69406d3a905677354983e357bba05
Daniel <oweiyco@gmail.com>
2013-10-28 22:59:17 -0400

    Homework 7 Done

--- Gemfile
+++ Gemfile
@@ -4,6 +4,9 @@ ruby '1.9.3'
 
 gem 'rails', '3.2.13'
 
+# Homework 7: add a gem
+gem 'aaronh-chronic'
+
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,4 +1,9 @@
+require 'chronic'
+
 class LandingPagesController < ApplicationController
   def home
+    @current = Time.now
+    @tomorrow = Chronic.parse("tomorrow")
+    @oneyearago = Chronic.parse("1 year ago")
   end
 end
